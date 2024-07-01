a785d4a7b81b70341368818aa886c385cab16f77
MysticBlaze085 <seerblazesolutions@gmail.com>
2024-07-01 18:38:27 -0500

    button corrections

--- react-workspace/packages/shared-ui/src/modules/components/general/Button.tsx
+++ react-workspace/packages/shared-ui/src/modules/components/general/Button.tsx
@@ -2,16 +2,16 @@ import { Button } from '@material-tailwind/react';
 import React from 'react';
 interface ButtonProps {
     children: React.ReactNode;
+    variant?: 'filled' | 'outlined' | 'gradient' | 'text';
     [key: string]: any;
   }
   
- const ButtonDefault: React.FC<ButtonProps> = (props) => {
+ const ButtonDefault: React.FC<ButtonProps | any> = ({variant = 'filled', ...props}) => {
+  const validVariant = ['filled', 'outlined', 'gradient', 'text'].includes(variant) ? variant : 'filled';
     return (
-      <Button 
+      <Button
         ripple={true}
-        placeholder=""
-        onPointerEnterCapture={() => {}}
-        onPointerLeaveCapture={() => {}}
+        variant={validVariant}
         {...props}>
         {props.children}
       </Button>
