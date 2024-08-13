0536507738ac7a4e23e7161ecb302fd73cc38f33
AzureAD\MelissaLanghausen <melissa.langhausen@rhapsody.health>
2024-08-13 17:34:06 -0500

    checkbox blue is back

--- packages/ng-ui/projects/ng-lib/src/lib/tw-form-ui/components/types/checkbox.component.ts
+++ packages/ng-ui/projects/ng-lib/src/lib/tw-form-ui/components/types/checkbox.component.ts
@@ -17,7 +17,7 @@ import { Field } from '../../models';
                 <label class="relative flex items-center p-3 rounded-full cursor-pointer">
                     <input
                         type="checkbox"
-                        class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                        class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
                         [checked]="formControl[field.key][option.id].value"
                         [name]="option.label"
                         [id]="option.id"
@@ -54,7 +54,7 @@ import { Field } from '../../models';
             <label class="relative flex items-center p-3 rounded-full cursor-pointer" for="checkbox">
                 <input
                     type="checkbox"
-                    class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                    class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
                     [checked]="formControl[field.key].value"
                     [name]="id"
                     [id]="id"
@@ -141,7 +141,7 @@ export class CheckboxComponent implements OnChanges, AfterViewInit {
     }
 
     get inputClass() {
-        return this.props?.inputClass ?? 'h-4 w-4 rounded border-gray-300 text-gray-600 focus:ring-gray-600';
+        return this.props?.inputClass ?? 'h-4 w-4 rounded border-blue-300 text-blue-600 focus:ring-blue-600';
     }
 
     ngAfterViewInit(): void {
